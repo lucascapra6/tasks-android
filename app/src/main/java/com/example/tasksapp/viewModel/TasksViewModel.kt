45fe12b7d41fd07127cb31370e57e6e4426f0716
Lucas de Capra <lucas.capra@mobilesaude.com.br>
2024-08-26 19:37:53 -0300

    implementa fluxo de fetch da task list all

--- app/src/main/java/com/example/tasksapp/viewModel/TasksViewModel.kt
+++ app/src/main/java/com/example/tasksapp/viewModel/TasksViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.devmasterteam.tasks.service.repository.remote.RetrofitClient
 import com.example.tasksapp.models.Tasks.TaskModel
+import com.example.tasksapp.models.ValidationApiModel
 import com.example.tasksapp.services.infra.SharedPreferences.SharedPreferencesTasksHelper
 import com.example.tasksapp.services.repository.remote.tasks.TasksRepository
 import com.example.tasksapp.utils.Constants
@@ -15,34 +16,12 @@ import kotlinx.coroutines.launch
 class TasksViewModel(application: Application) : AbstractViewModel(application) {
     private val sharedPreferencesTasksHelper = SharedPreferencesTasksHelper(application)
     private val repository = TasksRepository()
-    private val _tasks = MutableLiveData<List<TaskModel>>()
-    var tasks: LiveData<List<TaskModel>> = _tasks
+    private val _tasks = MutableLiveData<ValidationApiModel<List<TaskModel>>>()
+    private val _loadingTasks = MutableLiveData<Boolean>()
+    val tasks: LiveData<ValidationApiModel<List<TaskModel>>> = _tasks
+    val loadingTasks: LiveData<Boolean> = _loadingTasks
 
     fun fetchAllTasks() {
-        val mockTasks = listOf(
-            TaskModel(
-                id = "1",
-                priorityId = "1",
-                description = "Estudar Kotlin",
-                dueDate = "2024-09-01",
-                complete = false
-            ),
-            TaskModel(
-                id = "2",
-                priorityId = "2",
-                description = "Desenvolver novo projeto Android",
-                dueDate = "2024-09-10",
-                complete = false
-            ),
-            TaskModel(
-                id = "3",
-                priorityId = "3",
-                description = "Publicar aplicativo na Play Store",
-                dueDate = "2024-09-15",
-                complete = true
-            )
-        )
-
         val token = sharedPreferencesTasksHelper.get(Constants.SharedPreferencesKeys.TOKEN)
         val personKey = sharedPreferencesTasksHelper.get(Constants.SharedPreferencesKeys.PERSON_KEY)
 
@@ -50,10 +29,14 @@ class TasksViewModel(application: Application) : AbstractViewModel(application) 
 
         viewModelScope.launch {
             try {
+                _loadingTasks.value = true
                 val response = repository.fetchAllTasks()
-                _tasks.value = mockTasks
+                _tasks.value = ValidationApiModel(response, true)
             } catch (e: Exception) {
-                val error = e
+                _tasks.value = ValidationApiModel(null, false, "Falha ao carregar tarefas")
+            }
+            finally {
+                _loadingTasks.value = false
             }
         }
     }
