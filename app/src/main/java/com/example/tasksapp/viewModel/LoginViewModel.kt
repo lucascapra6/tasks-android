9a0b29c374922a5cb6646c3f309bee8cdcdc3b75
Lucas de Capra <lucas.capra@mobilesaude.com.br>
2024-08-26 19:39:17 -0300

    refatora view models

--- app/src/main/java/com/example/tasksapp/viewModel/LoginViewModel.kt
+++ app/src/main/java/com/example/tasksapp/viewModel/LoginViewModel.kt
@@ -1,28 +1,27 @@
 package com.example.tasksapp.viewModel;
 import android.app.Application
-import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData;
 import com.example.tasksapp.R
 import com.example.tasksapp.services.infra.SharedPreferences.SharedPreferencesTasksHelper
 import com.example.tasksapp.models.Auth.UserModel
 import com.example.tasksapp.services.interfaces.ApiListener
-import com.example.tasksapp.models.ValidateApiModel
+import com.example.tasksapp.models.ValidationModel
 import com.example.tasksapp.services.repository.remote.auth.AuthRepository
 import com.example.tasksapp.utils.Constants
 
 class LoginViewModel(application: Application) : AbstractViewModel(application) {
     private val sharedPreferencesTasksHelper = SharedPreferencesTasksHelper(application)
     private val authRepository = AuthRepository()
-    private val _isLoginSuccessful = MutableLiveData<ValidateApiModel>()
-    val loginResult: LiveData<ValidateApiModel> = _isLoginSuccessful
+    private val _isLoginSuccessful = MutableLiveData<ValidationModel>()
+    val loginResult: LiveData<ValidationModel> = _isLoginSuccessful
     private val _isUserLogged = MutableLiveData<Boolean>()
     val isUserLogged: LiveData<Boolean> = _isUserLogged
 
     fun doLogin(email: String, password: String) {
         authRepository.login(email, password, object: ApiListener<UserModel> {
             override fun onSuccess(response: UserModel) {
-                _isLoginSuccessful.value = ValidateApiModel(true, stringfyResource(R.string.success_login))
+                _isLoginSuccessful.value = ValidationModel(true, stringfyResource(R.string.success_login))
                 val token = response.token
                 val name = response.name
                 val personKey = response.personKey
@@ -33,7 +32,7 @@ class LoginViewModel(application: Application) : AbstractViewModel(application) 
             }
 
              override fun onFailure(t: Throwable) {
-                _isLoginSuccessful.value = ValidateApiModel(false, t.message)
+                _isLoginSuccessful.value = ValidationModel(false, t.message)
             }
         })
     }
--- app/src/main/java/com/example/tasksapp/viewModel/RegisterViewModel.kt
+++ app/src/main/java/com/example/tasksapp/viewModel/RegisterViewModel.kt
@@ -1,13 +1,12 @@
 package com.example.tasksapp.viewModel
 
 import android.app.Application
-import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.tasksapp.R
 import com.example.tasksapp.models.Auth.NewUserRegisterModel
 import com.example.tasksapp.models.Auth.UserModel
-import com.example.tasksapp.models.ValidateApiModel
+import com.example.tasksapp.models.ValidationModel
 import com.example.tasksapp.services.infra.SharedPreferences.SharedPreferencesTasksHelper
 import com.example.tasksapp.services.interfaces.ApiListener
 import com.example.tasksapp.services.repository.remote.auth.AuthRepository
@@ -16,8 +15,8 @@ import com.example.tasksapp.utils.Constants
 class RegisterViewModel(application: Application): AbstractViewModel(application) {
     private val authRepository = AuthRepository()
     private val sharedPreferencesTasksHelper = SharedPreferencesTasksHelper(application)
-    private val _createdUserSuccess = MutableLiveData<ValidateApiModel>()
-    val createdUserSuccess: LiveData<ValidateApiModel> = _createdUserSuccess
+    private val _createdUserSuccess = MutableLiveData<ValidationModel>()
+    val createdUserSuccess: LiveData<ValidationModel> = _createdUserSuccess
     fun registerUser(newUser: NewUserRegisterModel) {
         authRepository.register(newUser, object: ApiListener<UserModel> {
             override fun onSuccess(response: UserModel) {
@@ -25,11 +24,11 @@ class RegisterViewModel(application: Application): AbstractViewModel(application
                 sharedPreferencesTasksHelper.store(Constants.SharedPreferencesKeys.PERSON_KEY, response.personKey)
                 sharedPreferencesTasksHelper.store(Constants.SharedPreferencesKeys.TOKEN, response.token)
 
-                _createdUserSuccess.value = ValidateApiModel(true, stringfyResource(R.string.success_user_created))
+                _createdUserSuccess.value = ValidationModel(true, stringfyResource(R.string.success_user_created))
             }
 
             override fun onFailure(t: Throwable) {
-                _createdUserSuccess.value = ValidateApiModel(false, t.message)
+                _createdUserSuccess.value = ValidationModel(false, t.message)
             }
         })
     }
